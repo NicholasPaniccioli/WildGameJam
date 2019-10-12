11f82dcb22f0df8db1d2e8d23e6138c386069388
Nicholas Paniccioli <njp7745@g.rit.edu>
2019-10-12 13:03:14 -0400

    Made some Prefabs, Made a scene for testing, and added rigid and box colliders. starting jumping and player movement

--- EmpressChild/Assets/Scripts/CollisionDetection.cs
+++ EmpressChild/Assets/Scripts/CollisionDetection.cs
@@ -6,21 +6,32 @@ public class CollisionDetection : MonoBehaviour
 {
     //Fields
     //Dimensions For Collision
-    int minY;
-    int minX;
-    int maxY;
-    int maxX;
+    float minY;
+    float minX;
+    float maxY;
+    float maxX;
     GameObject attached;
+    Collider collider;
 
     // Start is called before the first frame update
     void Start()
     {
-       //attached = this GameObject;
+        //Starts getting the bounds of the GO
+        attached = this.gameObject;
+        collider = attached.GetComponent<Collider>();
+        minY = collider.bounds.min.y;
+        minX = collider.bounds.min.x;
+        maxY = collider.bounds.max.y;
+        maxX = collider.bounds.max.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Keeps updating the bounds
+        minY = collider.bounds.min.y;
+        minX = collider.bounds.min.x;
+        maxY = collider.bounds.max.y;
+        maxX = collider.bounds.max.x;
     }
 }
---
+++ EmpressChild/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private bool grounded; //Is the player on the ground
+    private Rigidbody2D rb; //The RB of the GO
+    public float jumpForce; //The power the player has on their jump
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("a") && grounded == true)
+        {
+
+        }
+    }
+}
