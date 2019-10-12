beae55b6e6e95392998f5c7839a03d2c296a54bf
Kippy Tidwell <kct2548@rit.edu>
2019-10-12 17:37:35 -0400

    Added an example of how to add a dialogue to a trigger

---
+++ EmpressChild/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public DialogueManager dialogueManager;
+    public bool aldreadyTriggered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueManager = DialogueManager.Instance();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!aldreadyTriggered)
+        {
+            dialogueManager.MediumDialogueBox("Test 1");
+            aldreadyTriggered = true;
+        }
+    }
+}
--- EmpressChild/Assets/Scripts/PlayerMovement.cs
+++ EmpressChild/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     public Vector3 jumpVec; //Vector for the jumpforce to be applied to
     public bool grounded; //Is the player on the ground
 
+    public float speed = 100f;
 
     private BoxCollider2D pCollider;
 
@@ -18,8 +19,10 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        jumpForce = 7.0f;
-        jumpVec = new Vector3(0.0f, 3.0f, 0.0f);
+        jumpForce = 10.0f;
+        jumpVec = new Vector3(0.0f, 6.0f, 0.0f);
+
+        
 
         pCollider = GetComponent<BoxCollider2D>(); 
     }
@@ -34,6 +37,12 @@ public class PlayerMovement : MonoBehaviour
             rb.AddForce(jumpVec * jumpForce, ForceMode2D.Impulse);
             grounded = false; //Since they jumped they are no longer grounded
         }
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        rb.AddForce(new Vector3(input.x * speed, 0f, 0f), ForceMode2D.Force);
+        
+        /*
         if(Input.GetKey(KeyCode.A)) //LEFT
         {
             rb.AddForce(new Vector3(-2.0f, 0.0f, 0.0f) * 20, ForceMode2D.Force);
@@ -42,14 +51,16 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.AddForce(new Vector3(2.0f, 0.0f, 0.0f) * 20, ForceMode2D.Force);
         }
+        */
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
         print("collided!" + collision.gameObject.tag);
         print((pCollider.transform.position.y + pCollider.size.y).ToString() + collision.transform.position.y);
         //check to see if the collider is a platfrom, and make sure it didn't collide with the side of the platfrom
+        print("min pcollider: " + pCollider.bounds.min.y + " max collider: " + collision.collider.bounds.max.y);
         if(collision.gameObject.tag == "Platform" && 
-            pCollider.bounds.min.y >= collision.collider.bounds.max.y)
+            pCollider.bounds.min.y >= collision.collider.bounds.max.y - .02f)
         {
             grounded = true;
         }
