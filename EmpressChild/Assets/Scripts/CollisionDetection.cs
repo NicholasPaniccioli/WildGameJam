032c29712256d08ff4cb9817924be1ddff308d99
Nicholas Paniccioli <njp7745@g.rit.edu>
2019-10-12 11:43:26 -0400

    Made a scene for testing out collision, started work on the collision script

---
+++ EmpressChild/Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    //Fields
+    //Dimensions For Collision
+    int minY;
+    int minX;
+    int maxY;
+    int maxX;
+    GameObject attached;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       //attached = this GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
