729d3e8861d7784e722a97e0e2f2b96bf29b6cc4
Nicholas Paniccioli <njp7745@g.rit.edu>
2019-10-13 11:30:21 -0400

    Fixed the Parallax, Not so smooth but it works.

--- EmpressChild/Assets/Scripts/ParallaxScroll.cs
+++ EmpressChild/Assets/Scripts/ParallaxScroll.cs
@@ -12,28 +12,25 @@ public class ParallaxScroll : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         startPos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
-        print("Length" + length);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        float distance = (cam.transform.position.x * parallax);
-        float temp = (cam.transform.position.x * parallax);
+        float distance = (cam.transform.position.x * parallax); 
+        float temp = (cam.transform.position.x * (1 - parallax));
         transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
 
 
-        if(temp > startPos + length)
-        {
-            startPos += length;
-        }
-        else if(temp < startPos - length)
-        {
-            startPos -= length;
-        }
+       if(temp > startPos + length)
+       {
+           startPos += length;
+       }
+       else if(temp < startPos - length)
+       {
+           startPos -= length;
+       }
     }
 }
