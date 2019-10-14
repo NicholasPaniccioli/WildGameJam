9e7b639e7814d11463553051e5a558d125096be3
Vestroie <heroprotunlong@gmail.com>
2019-10-14 10:12:30 -0400

    Add SFX
    Added ambience sounds to Forest and City Scenes.
    Fixed scaling in city scenes

---
+++ EmpressChild/Assets/Random_Receptionist.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Random_Receptionist : MonoBehaviour
+{
+    public Sprite[] receptionistSprites;
+
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+        int rand = Random.Range(0, 6);
+
+        sr.sprite = receptionistSprites[rand];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
